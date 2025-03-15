6e4f6d9043dbcc19e9c0256b5c630ed23a9f55ab
Dingo545-alt <linyuic@uci.edu>
2025-03-15 01:45:19 +0000

    path should be ok now

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -48,9 +48,8 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total){
-    for (size_t i = 0; i < v.size(); ++i) {
-        if (i > 0) cout << " ";
-        cout << v[i];
+    for (auto vertex : v) {
+        cout << vertex << " ";
     }
-    cout << "\nTotal cost is "<<total << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
