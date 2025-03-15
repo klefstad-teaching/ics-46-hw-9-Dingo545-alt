9bb47ae49b16fa1e1f61f5ae21c142882c3b8998
Dingo545-alt <linyuic@uci.edu>
2025-03-15 01:26:21 +0000

    string fromat issue dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -58,5 +58,5 @@ void print_path(const vector<int>& v, int total){
         if (i != v.size() - 1)
             cout << " ";
     }
-    cout << "\nTotal cost is  "<<total << endl;
+    cout << "\nTotal cost is "<<total << endl;
 }
